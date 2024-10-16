b77cd084f37a271b0a909497a9f0561b9df516e3
papitoulini <113286556+Papitoulini@users.noreply.github.com>
2024-10-16 16:47:47 +0300

    minor fixes

--- scripts/sum.js
+++ scripts/sum.js
@@ -1,7 +1,7 @@
-export function sum(...numbers: number[]) {
+export function sum(...numbers) {
   return numbers.reduce((x, y) => x + y);
 }
 
-export async function asyncSum(...numbers: number[]) {
+export async function asyncSum(...numbers) {
   return new Promise(resolve => resolve(numbers.reduce((x, y) => x + y)));
 }
